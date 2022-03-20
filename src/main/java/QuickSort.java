df6f383a46f47039c1f16eac943384ea705bf4a9
NoraNekoIT <bunsmart18@gmail.com>
2022-03-20 13:40:36 +0700

    update wrong name file

---
+++ src/main/java/QuickSort.java
@@ -0,0 +1,14 @@
+public class QuickSort {
+    public static void main(String[] args) {
+
+    }
+
+    public static class SelectionSort {
+        /*
+
+         */
+        public static void main(String[] args) {
+
+        }
+    }
+}
--- src/main/java/Search/SelectionSearch.java
+++
@@ -1,8 +0,0 @@
-package Search;
-
-public class SelectionSearch {
-
-    public static void main(String[] args) {
-
-    }
-}
