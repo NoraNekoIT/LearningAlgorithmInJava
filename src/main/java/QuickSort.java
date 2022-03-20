da3730fb7ce80da955ca69ed015d8291dd743ebb
NoraNekoIT <bunsmart18@gmail.com>
2022-03-20 14:09:51 +0700

    update

--- src/main/java/QuickSort.java
+++ src/main/java/QuickSort.java
@@ -1,6 +1,7 @@
 public class QuickSort {
     /*
     Lebih capat dari Selection sort
+    dengan nilai tengah pivot menyimpan sortKiri dan kanan
     * */
     public static void main(String[] args) {
 
