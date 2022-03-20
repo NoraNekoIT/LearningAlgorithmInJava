1308489c6f0b4301b48e0d9f36f80cfad786a3e8
NoraNekoIT <bunsmart18@gmail.com>
2022-03-20 12:19:17 +0700

    Binary Search and SimpleSearchWay

---
+++ src/main/java/BinarySearch.java
@@ -0,0 +1,11 @@
+public class BinarySearch {
+    /*
+        Dimulai dari tengah,
+        Misal 1-100
+        50,75,63,57
+        100--> 50-->25-->13-->7-->4-->2-->1
+    */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/SimpleSearchWay.java
@@ -0,0 +1,7 @@
+public class SimpleSearchWay {
+    /*algoritma dari awal sampai akhir sampai yg dicari ditemukan;
+    this is stupid search*/
+    public static void main(String[] args) {
+
+    }
+}
