ed194fb91e228bf9bb3acdecf937bc01b959606a
NoraNekoIT <bunsmart18@gmail.com>
2022-03-20 12:20:50 +0700

    add package search

--- src/main/java/Search/BinarySearch.java
+++ src/main/java/Search/BinarySearch.java
@@ -1,9 +1,11 @@
+package Search;
+
 public class BinarySearch {
     /*
         Dimulai dari tengah,
         Misal 1-100
         50,75,63,57
-        100--> 50-->25-->13-->7-->4-->2-->1
+        100--> 50-->25-->13-->7-->4-->2-->1 (only 7 step)
     */
     public static void main(String[] args) {
 
--- src/main/java/Search/SimpleSearchWay.java
+++ src/main/java/Search/SimpleSearchWay.java
@@ -1,3 +1,5 @@
+package Search;
+
 public class SimpleSearchWay {
     /*algoritma dari awal sampai akhir sampai yg dicari ditemukan;
     this is stupid search*/
